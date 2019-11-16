4c5fce200f90678cd94a3a8313f7a5005c29c798
Sk Tania Afrin <Tania@Sks-MacBook-Pro.lehman.edu>
2019-11-15 21:09:04 -0500

    routes

--- lehmansys/src/App.js
+++ lehmansys/src/App.js
@@ -1,26 +1,26 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, Switch} from 'react-router-dom'
+import Home from './Components/Home/Home';
+import Welcome from './Components/Welcome/Welcome';
+//import Test from './Test/';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends React.Component{
+  constructor(props){
+    super();
+    
+  }
+  render(){
+    const welcomePageComponent = () => <Welcome/>;
+    const homePageComponent = () => <Home/>;
+    return (
+      <div className="App">
+        <Router>
+          <div>
+            <Route exact path='/' render = {welcomePageComponent}></Route>
+            <Route exact path='/home' render = {homePageComponent}></Route>
+          </div>
+        </Router>
+      </div>
+    );
+  }
 }
-
-export default App;
